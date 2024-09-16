fb0295db4dd9798c9f835b13efa1752116c0726e
owls4all <owls4all@gmail.com>
2024-09-16 08:59:51 -0700

    Solved excercise

--- main.py
+++ main.py
@@ -1,1 +1,18 @@
 import turtle
+#
+#-setting up shortcuts-#
+t=turtle
+def L():
+    t.left(90)
+def go(d):
+    t.forward(d)
+def R():
+    t.right(90)
+#----------------------#
+side=20
+R()
+for i in range(1,13):
+    go(i*side)
+    R()
+
+t.exitonclick()
